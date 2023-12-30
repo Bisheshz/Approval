f3ada129feb6deed838bd0cb53a40ad63cd7877c
Bishesh <153376049+LordBishesh@users.noreply.github.com>
2023-12-30 17:05:55 +0545

    Update Bishesh.py

--- Bishesh.py
+++ Bishesh.py
@@ -1,39 +1,25 @@
 import os
 import requests
 import sys
-import time
 
-protocol = 'https'
-host = 'github'
-username = 'LordBishesh'
-repository = 'Bisheshz'
-file_path = 'a.txt'
-
-def check_id_key():
-    uid = str(os.geteuid())
-    login = str(os.getlogin())
-    id_key = f"BISHESH → {uid} → {login}"
-    print("ID Key:", id_key)
-
-    input("Press Enter to continue...")
-
-    while True:
-        url = f'{protocol}://{host}.com/{username}/{repository}/blob/main/{file_path}'
-        try:
-            server = requests.get(url).text
-            print("Content retrieved:", server)
-
-            if "Id" in server and id_key in server:
-                print("Thanks for purchasing my tool.")
-                sys.exit()
-            else:
-                print("Id key not found. Exiting...")
-                sys.exit()
-
-        except requests.RequestException as e:
-            print(f"Error: {e}")
+def ckx():
+    bis = 'https'
+    he = 'github'
+    sh = 'LordBishesh'
+    
+    uuid = "BISHESH" + str(os.geteuid()) + str(os.getlogin())
+    id = "→".join(uuid)
+    print("ID:", id)
+    
+    try:
+        server = requests.get(f'{bis}://{he}.com/{sh}/Bisheshz/raw/main/a.txt').text
+        httpCaht = requests.get(f"{bis}://{he}.com/{sh}/Bisheshz/raw/main/b.txt").text
         
-        time.sleep(1)  # Adjust the sleep time as needed
-
-# Call the function
-check_id_key()
+        if id in server and id in httpCaht:
+            msg = str(os.geteuid())
+            pass
+        else:
+            msg = str(os.geteuid())
+            #fucked()
+    except:
+        sys.exit()
