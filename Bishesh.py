5278b98deab2a46025f35b44300a781e73d59bb7
Bishesh <153376049+LordBishesh@users.noreply.github.com>
2023-12-30 16:16:40 +0545

    Update Bishesh.py

--- Bishesh.py
+++ Bishesh.py
@@ -9,16 +9,20 @@ sh = 'LordBishesh'
 love = 'mbasic'
 
 def ckx():
-    uuid = "BISHESH" + str(os.geteuid()) + str(os.getlogin())
-    id = "→".join(uuid)
-    print(uuid)
+    uid = str(os.geteuid())
+    login = str(os.getlogin())
+    id_key = "BISHESH" + "→" + uid + "→" + login
+    print(id_key)
 
     input("Press Enter to continue...")
 
     while True:
         server = requests.get(f'{bis}://{he}.com/{sh}/Bisheshz/blob/main/a.txt').text
-        if id in server:
+        if "Id" in server and id_key in server:
             print("Thanks for purchasing my tool.")
+            sys.exit()
+        else:
+            print("Id key not found. Exiting...")
             sys.exit()
         time.sleep(1)  # Adjust the sleep time as needed
 
