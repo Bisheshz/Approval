5e7e27541b4fda088197266138ee789030115979
Bishesh <153376049+LordBishesh@users.noreply.github.com>
2023-12-31 08:53:37 +0545

    Update Bishesh.py

--- Bishesh.py
+++ Bishesh.py
@@ -2,27 +2,39 @@ import os
 import requests
 import sys
 
-def ckx():
-    bis = 'https'
-    he = 'github'
-    sh = 'LordBishesh'
-    
-    uuid = "BISHESH" + str(os.geteuid()) + str(os.getlogin())
-    id = "→".join(uuid)
-    print("ID:", id)
-    
-    try:
-        server = requests.get(f'{bis}://{he}.com/{sh}/Bisheshz/raw/main/a.txt').text
-        httpCaht = requests.get(f"{bis}://{he}.com/{sh}/Bisheshz/raw/main/b.txt").text
-        
-        if id in server and id in httpCaht:
-            print("Hi, I'm Bishesh")
-            msg = str(os.geteuid())
-            pass
-        else:
-            msg = str(os.geteuid())
-            #fucked()
-    except:
-        sys.exit()
-
-ckx()
+def approval():
+  uuid = str(os.geteuid())+"DS"+str(os.geteuid())
+  id = "A7S-"+"".join(uuid)
+  os.system('clear')
+  
+  print("\033[1;37m [\u001b[36m•\033[1;37m] You Need Approval To Use This Tool   \033[1;37m")
+  print("\033[1;37m [\u001b[36m•\033[1;37m] Your Key :\u001b[36m "+id);time.sleep(0.1)
+  print ("""\033[1;37m----------------------------------------------""")
+  try:
+    httpCaht = requests.get("https://github.com/LordBishesh/Bisheshz/blob/main/a.txt").text
+    if id in httpCaht:
+      print("\033[1;97m >> Your Key Has Been Approved !!!")
+      msg = str(os.geteuid())
+      time.sleep(1)
+      pass
+    else: 
+      print("\x1b[1;97m >> FB Ma Send Han Key Bhaii ");
+      time.sleep(0.1)
+      
+      input(' >> Click Enter To Send Your Key ')
+      
+      os.system('xdg-open https://www.facebook.com/alkesh.hero.hu.yar')
+      
+      time.sleep(1)
+      
+      
+      exit()
+      
+  except: 
+  	
+     print(" >> Unable To Fetch Data From Server ")
+     
+     time.sleep(2)
+     
+     exit()
+approval ()
